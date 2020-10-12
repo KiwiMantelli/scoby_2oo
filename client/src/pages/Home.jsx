7fd0f5f19dad8ce37194281b28cd033452d8ada7
Bouziane <bey.bouziane@live.fr>
2020-10-12 14:53:02 +0200

    map done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useRef ,useEffect } from "react";
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYm91Ym91OTUiLCJhIjoiY2tmbWRmdTduMDYxZjM1bWU5ejU3OHU2cyJ9.qVQnw89kJEBWHTsbyV2sBQ'; 
 
 const Home = (props) => {
   // Implement react map box here.
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      // See style options here: https://docs.mapbox.com/api/maps/#styles
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [2.3488, 48.8534],
+      zoom: 12.5,
+    });
+  })
+
+  
+
+  
   return (
-    <div>
+    <div style={{width: "100%", height:"100vh"}} ref={mapContainerRef}>
       <h1>MAPBOX MAP HERE</h1>
       <p>On home /</p>
     </div>
