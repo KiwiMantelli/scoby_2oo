c7e9e24fcc757a0dd02a83839dc1974d3db1c087
Bouziane <bey.bouziane@live.fr>
2020-10-13 16:48:28 +0200

    Merge conflict ?

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,27 +1,16 @@
 import React from "react";
 import ReactMapboxGl, { Marker } from "react-mapbox-gl";
 
-
-
-
 const Map = ReactMapboxGl({
   accessToken: process.env.REACT_APP_MAPBOX_TOKEN
 });
 
-
 const Home = (props) => {
-  // Implement react map box here.
-  
 
-  
-  
-
-  
   return (
     <Map style="mapbox://styles/mapbox/light-v10"  center={[2.3488, 48.85341]}  containerStyle={{
       height: '100vh',
       width: '100vw',
-     
     }}>
 <Marker
   coordinates={[2.416815, 48.5285582]}
